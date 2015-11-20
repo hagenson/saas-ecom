ca981ff341ba376ad7f72525c8dd4a3a9ae57bb8
hagenson <github@hagenson.com>
2015-11-20 17:22:52 +1300

    Added internal subscription provide

---
+++ SaasEcom.Core/Infrastructure/PaymentProcessor/Internal/InternalSubscriptionProvider.cs
@@ -0,0 +1,50 @@
+﻿using SaasEcom.Core.Infrastructure.PaymentProcessor.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasEcom.Core.Infrastructure.PaymentProcessor.Internal
+{
+    /// <summary>
+    /// Implements the ISubscriptionProvider using the internal database.
+    /// </summary>
+    public class InternalSubscriptionProvider : ISubscriptionProvider
+    {
+        public string SubscribeUser(Models.SaasEcomUser user, string planId, int trialInDays = 0, decimal taxPercent = 0)
+        {
+            return null;
+        }
+
+        public string SubscribeUser(Models.SaasEcomUser user, string planId, DateTime? trialEnds, decimal taxPercent = 0)
+        {
+            return null;
+        }
+
+        public Task<List<Models.Subscription>> UserSubscriptionsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime EndSubscription(string userStripeId, string subStripeId, bool cancelAtPeriodEnd = false)
+        {
+            return DateTime.Now;
+        }
+
+        public bool UpdateSubscription(string customerId, string subStripeId, string newPlanId, bool proRate)
+        {
+            return true;
+        }
+
+        public bool UpdateSubscriptionTax(string customerId, string subStripeId, decimal taxPercent = 0)
+        {
+            return true;
+        }
+
+        public object SubscribeUserNaturalMonth(Models.SaasEcomUser user, string planId, DateTime? billingAnchorCycle, decimal taxPercent)
+        {
+            return null;
+        }
+    }
+}
